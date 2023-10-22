53331b4233c3efde9cff7e7ef4d386ac2594d065
Victor Alcaraz <vctralcaraz@gmail.com>
2023-10-21 18:02:59 -0700

    complete

--- merge-sort.js
+++ merge-sort.js
@@ -3,15 +3,24 @@
 function mergeSort(arr) {
 
   // Check if the input is length 1 or less
+  if(arr.length <= 1) {
     // If so, it's already sorted: return
+    return arr;
+  }
 
   // Divide the array in half
+  let half = Math.ceil(arr.length / 2);
+  let left = arr.slice(0, half);
+  let right = arr.slice(half);
 
   // Recursively sort the left half
+  let one = mergeSort(left);
+
   // Recursively sort the right half
+  let two = mergeSort(right);
 
   // Merge the halves together and return
-
+  return merge(one, two);
 }
 
 
@@ -19,15 +28,37 @@ function mergeSort(arr) {
 function merge(arrA, arrB) {
 
   // Create an empty return array
+  let newArr = [];
 
   // Point to the first value of each array
+  let aPointer = 0;
+  let bPointer = 0;
+
   // While there are still values in each array...
+  while(aPointer + bPointer < arrA.length + arrB.length) {
     // Compare the first values of each array
-    // Add the smaller value to the return array
-    // Move the pointer to the next value in that array
+    let left = arrA[aPointer];
+    let right = arrB[bPointer];
+
+    if(left < right || bPointer === arrB.length) {
+      // Add the smaller value to the return array
+      newArr.push(left);
+
+      // Move the pointer to the next value in that array
+      aPointer++;
+
+    } else {
+      // Add the smaller value to the return array
+      newArr.push(right);
+
+      // Move the pointer to the next value in that array
+      bPointer++;
+
+    }
+  }
 
   // Return the return array
-
+  return newArr;
 }
 
 module.exports = [merge, mergeSort];
